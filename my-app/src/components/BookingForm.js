b4741c82315d83f7477c1ee12d608c374acf2b4e
Kostas Stathakos <dev@e-leven.net>
2023-11-23 20:12:59 -0500

    Lift avalibale times state into parent and use useReducer for it

--- my-app/src/components/BookingForm.js
+++ my-app/src/components/BookingForm.js
@@ -1,39 +1,41 @@
 import { useState } from "react"
+import { SET } from '../state/availableTimesReducer.js'
 
-const BookingForm = () => {
- const [date, setDate] = useState("")
- const [time, setTime] = useState("")
- const [guestNumber, setGuestNumber] = useState("")
- const [occasion, setOccasion] = useState("")
+const BookingForm = (props) => {
+    const [date, setDate] = useState("")
+    const [time, setTime] = useState("")
+    const [guestNumber, setGuestNumber] = useState("")
+    const [occasion, setOccasion] = useState("")
 
- const getIsFormValid = () => {
-   return (
-     date && time && guestNumber
-   )
- }
- 
- const clearForm = () => {
-   setDate("")
-   setTime("")
-   setGuestNumber("")
-   setOccasion("")
- }
+    const getIsFormValid = () => {
+        return (
+          date && time && guestNumber
+        )
+      }
 
- const handleSubmit = (e) => {
-   e.preventDefault()
-   alert("Booking created!")
-   clearForm()
- }
+      const clearForm = () => {
+        setDate("")
+        setTime("")
+        setGuestNumber("")
+        setOccasion("")
+      }
 
- return (
-   <div className="BookingForm">
-     <form onSubmit={handleSubmit}>
-       <fieldset>
-         <h1>Reserve a Table</h1>
-         <div className="Field">
-           <label>Date <sup>*</sup> </label>
-           <input
-             type="date"
+      const handleSubmit = (e) => {
+        e.preventDefault()
+        props.dispatch({ type: SET, value: time })
+        alert("Booking created!")
+        clearForm()
+      }
+
+      return (
+        <div className="BookingForm">
+          <form onSubmit={handleSubmit}>
+            <fieldset>
+              <h1>Reserve a Table</h1>
+              <div className="Field">
+                <label>Date <sup>*</sup> </label>
+                <input
+                  type="date"
              value={date}
              onChange={(e) => {
                setDate(e.target.value)
@@ -44,12 +46,9 @@ const BookingForm = () => {
          <div className="Field">
            <label>Time</label>
            <select value={time} onChange={(e) => setTime(e.target.value)}>
-             <option value="17">17:00</option>
-             <option value="18">18:00</option>
-             <option value="19">19:00</option>
-             <option value="20">20:00</option>
-             <option value="21">21:00</option>
-             <option value="22">22:00</option>
+            {props.availableTimes.map((time, index) => (
+                <option key={index} value={time}>{time}</option>
+            ))}
            </select>
          </div>
          <div className="Field">
--- my-app/src/pages/BookingPage.js
+++ my-app/src/pages/BookingPage.js
@@ -1,12 +1,16 @@
+import { useReducer } from "react";
 import { useLocation } from "react-router-dom";
 import MainLayout from '../layouts/Main.js'
 import BookingForm from '../components/BookingForm.js'
+import availableTimesReducer from '../state/availableTimesReducer.js'
 
 const BookingPage = () => {
+    const init = [ '17:00','18:00','19:00','20:00', '21:00', '22:00'];
+    const [state, dispatch] = useReducer(availableTimesReducer, init);
     const location = useLocation();
     return (
         <MainLayout location={location.pathname}>
-            <BookingForm />
+            <BookingForm dispatch={dispatch} availableTimes={state}/>
         </MainLayout>
         )
   };
